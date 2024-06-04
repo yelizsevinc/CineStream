e4d7fe45593799236de78445a8cf07487f215db7
yelizsevinc <yelizondessevinc@gmail.com>
2024-06-04 09:57:31 +0200

    add AppUser class to show PlatformUsers

---
+++ CineStream/Data/AppUser.cs
@@ -0,0 +1,7 @@
+public class AppUser
+{
+    public int UserId { get; set; }
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public DateTime? SubscriptionDate { get; set; }
+}
