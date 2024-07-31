1b6a811b1f171ed27de8829dce23019bde7fad92
yelizsevinc <yelizondessevinc@gmail.com>
2024-07-31 16:26:15 +0200

    User: refactoring & better error handling

--- CineStream/Data/AppUserShow.cs
+++
@@ -1,8 +0,0 @@
-public class AppUserShow
-{
-    public int ShowId { get; set; }
-    public string? Title { get; set; }
-    public DateTime? ReleaseDate { get; set; }
-    public bool? Watched { get; set; }
-    public DateTime? CreatedAt { get; set; }
-}
